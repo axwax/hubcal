48810380ca26525098ec4373ce3079c41479ed57
Axel Minet <axel@gigx.co.uk>
2015-11-10 14:48:23 +0000

    store-feeds: added ccf & ccv, hardcoded category 1

--- store-feeds.php
+++ store-feeds.php
@@ -5,6 +5,11 @@ require '../includes/db-functions.php';
 db_auth();
 
 $feedURLs = array();
+//$feedURLs['Cambridge Conservation Forum'] = 'http:/://www.cambridgeconservationforum.org.uk/calendar/ical/2015-11/calendar.ics';
+$feedURLs['Cambridge Conservation Volunteers'] = 'https://www.google.com/calendar/ical/cbhiefng3frcml9u64n3c4refk%40group.calendar.google.com/public/basic.ics';
+
+
+/*
 $feedURLs['PiNGS'] = 'http://groupspaces.com/PiNGS/api/events?alt=ical';
 $feedURLs['Afrinspire'] = 'http://groupspaces.com/AFRINSPIRE/api/events?alt=ical';
 $feedURLs['Centre for Global Equality'] = 'http://centreforglobalequality.org/calendar/list/?ical=1&tribe_display=list';
@@ -12,11 +17,12 @@ $feedURLs['Cambridge Hub'] = 'https://www.google.com/calendar/ical/cambridge.gre
 $feedURLs['Cambridge Carbon Footprint'] = 'http://cambridgecarbonfootprint.org/?ec3_ical';
 $feedURLs['Cambridge Sustainable Food City'] = 'http://www.cambridgesustainablefood.org/events/?ical=1&tribe_display=list';
 $feedURLs['Transition Cambridge'] = 'http://www.meetup.com/Transition-Cambridge/events/ical/';
+*/
 
 $feedcount = 0;
 foreach ($feedURLs as $name => $url){
   //$feed = db_select('feeds', array('*'), array('name' => $feedName ));
-  $aFields = array ('name' => $name, 'url' => $url);
+  $aFields = array ('name' => $name, 'url' => $url, 'category' => 1);
   db_insert('feeds', $aFields); 
   $feedcount++;
 }
