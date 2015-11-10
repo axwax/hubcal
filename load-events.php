2968608782e9e7de66e66e9a4d6f7b3176553553
Axel Minet <axel@gigx.co.uk>
2015-11-10 14:50:30 +0000

    load-events: if no requested feeds return empty array

--- load-events.php
+++ load-events.php
@@ -12,14 +12,16 @@ $end = date("Y-m-d", strtotime($_POST['end'] . " +1 month"));
 $category=1;
 $where = '';
 if($requestedFeeds){
-  //print_r(implode(',',array_keys($requestedFeeds)));
   $requestedFeedIDs = array_keys($requestedFeeds);
-  //$feedIDs = implode(',',$requestedFeedIDs);
   $where = "`id` IN (" . implode(',', $requestedFeedIDs) . ")"; // TODO: Sanitise!
+}
+else{
+  die('[]');
 }
 if($category){
   $where .=" AND `category` = '$category'";
 }
+
 
 $feeds = db_select('feeds', array('*'), $where);
 $feeds = $feeds['result'];
@@ -33,22 +35,16 @@ $feedIDs = array_keys($theFeeds);
 if($requestedFeeds){
   $feedIDs = array_intersect($feedIDs, $requestedFeedIDs);
 }
-//print_r($requestedFeedIDs);
-//print_r($feedIDs);
-//die;
 
 $where = '';
 if($start && $end){
   $where = "`start` > '$start' AND `end` < '$end'";
 }
-//if($requestedFeeds){
   $where .= " AND `feedID` IN (" . implode(',', $feedIDs) . ")";
-//}
 
 
-if(!$requestedFeeds) die('[]');
 
-//print_r($where);die;
+
 
 $events = db_select('events', array('*'), $where, array('start' => 'ASC'));
 $events = $events['result'];
