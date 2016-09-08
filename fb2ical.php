a6e0088fb6455f07376fcdb7f82789e07445a78a
Axel Minet <axel@gigx.co.uk>
2016-09-08 17:42:49 +0100

    added fb2ical

---
+++ fb2ical.php
@@ -0,0 +1,127 @@
+<?php
+
+require_once('iCalcreator/iCalcreator.php');
+
+$fb_id = $_GET['id'];
+$is_group = $_GET['group'];
+$path = '/var/www/vhosts/gigx.co.uk/green-calendar.gigx.co.uk/export/';
+$file = hash('md5',$fb_id).'.ics';
+$tz     = "Europe/London"; // define time zone
+$uid = "green-calendar.gigx.co.uk"; // set a (site) unique id
+$config = array( "unique_id" => $uid
+               , "TZID"      => $tz );
+               
+// load the cached version?
+if(file_exists($path.$file) && (time() - filemtime($path.$file) <3600)){
+  header('Content-type: text/calendar; charset=utf-8');
+  header('Content-Disposition: attachment; filename=' . $file);
+  readfile($path.$file);
+  die;
+}
+
+
+// get facebook events for group or page
+if($is_group) {
+  $access_token = "CAACEdEose0cBACi97Nj4X9chYT1N0Rrr1zwhmyDZBTZChuXPMqJCmd8pUnA6zaqeFH0RCWL1NfiNzHFig2GuHCpYYUPxrvvBZAX9BZASVF1pZAYlCFCLcLVxyUzwYhNHhz1EejEodFI8kecqlCjkszD4JxwGvwiSPTGSKKaOeQpZAtyRZCgtNZA3ipnuW1KmEH2Ar4TlnsFVZCW8cQWH4dxjM";
+  $outArr = get_graph($fb_id."/events/");
+  $events = $outArr['data'];
+}
+else {
+  $access_token = "187948597266|77843cfacc8f952ea5f00fcd802182ba";
+  $outArr = get_graph($fb_id,'events');
+  $events = $outArr['events']['data'];
+}
+
+// stop if we have no events
+if(empty($events)) die;
+
+// get Page/Group name
+$outArr = get_graph($fb_id, 'name,description');
+$calendarName = $outArr['name'];
+$calendarDesc = $outArr['description'];
+
+// sort events by date
+$sortedEvents = array();
+foreach($events as $event){
+  $sortedEvents[$event['start_time']] = $event;
+}
+ksort($sortedEvents);
+
+// start calendar output
+  // opt. "calendar" timezone
+$v      = new vcalendar( $config );
+  // create a new calendar instance
+$v->setProperty( "method", "PUBLISH" );
+  // required of some calendar software
+$v->setProperty( "x-WR-CALNAME", $calendarName );
+  // required of some calendar software
+$v->setProperty( "X-WR-CALDESC", $calendarDesc );
+  // required of some calendar software
+$v->setProperty( "X-WR-TIMEZONE", $tz );
+  // required of some calendar software
+$xprops = array( "X-LIC-LOCATION" => $tz );
+  // required of some calendar software
+iCalUtilityFunctions::createTimezone( $v, $tz, $xprops );
+  // create timezone component(-s) opt. 1
+  // based on present date
+
+// loop through events
+$out = '';
+foreach($sortedEvents as $event){
+  $title = $event['name'];
+  $start = $event['start_time'];
+  $end = $event['end_time'];
+  $id = $event['id'];
+  $location = $event['location'];
+  $detailsArr = get_graph($id,'cover,description');
+  $description = $detailsArr['description'];  
+  $image = $detailsArr['cover']['source'];
+  $url = "https://www.facebook.com/events/$id/";
+  
+  // new iCal Event:  
+  $vevent = & $v->newComponent( "vevent" ); // create an event calendar component
+  $vevent->setProperty( "dtstart", dateArray(strtotime($start)) );
+  if($end) $vevent->setProperty( "dtend",   dateArray(strtotime($end)) );
+  $vevent->setProperty( "LOCATION", $location );
+  $vevent->setProperty( "summary", $title );
+  $vevent->setProperty( "description", $description );
+  $vevent->setProperty( "comment", $comment );
+  $vevent->setProperty( "ATTACH" , $image );
+  $vevent->setProperty( "URL" , $url );
+  $vevent->setProperty( "UID" , $start."-".$id."@".$uid);
+}
+
+// save & output the iCal file
+$v->setConfig("directory", "export");
+$v->setConfig("filename", $file);
+$v->saveCalendar();
+  header('Content-type: text/calendar; charset=utf-8');
+  header('Content-Disposition: attachment; filename=' . $file);
+  readfile($path.$file);  
+
+
+// get graph object/fields via curl
+function get_graph($object,$fields=false){
+  global $access_token;
+  $graph_url= "https://graph.facebook.com/$object" . ($fields ? "?fields=$fields&access_token=" : "?access_token=") . $access_token;
+  //echo $graph_url;die;
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $graph_url);
+  curl_setopt($ch, CURLOPT_HEADER, 0);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+  $output = curl_exec($ch);
+  curl_close($ch);
+  return json_decode($output,true);;  
+}
+
+// iCal functions
+function dateArray($timestamp){ 
+  $dateArray = array( "year"  => date('Y',$timestamp)
+                    , "month" => date('m',$timestamp)
+                    , "day"   => date('d',$timestamp)
+                    , "hour"  => date('H',$timestamp)
+                    , "min"   => date('i',$timestamp)
+                    , "sec"   => date('s',$timestamp) );
+  return $dateArray;
+}
