36f514c7ded6c507c7d7fed0b452ac8e5b4d589a
Axel Minet <axel@gigx.co.uk>
2016-09-13 10:32:32 +0100

    added hubconfig.example file in includes folder - needs to be filled in and renamed to hubconfig.php

---
+++ includes/hubconfig.example.php
@@ -0,0 +1,10 @@
+<?php
+
+// fill in the below variables and rename the file to hubconfig.php
+
+$db = "";                                 // database name
+$user = "";                               // database username
+$pw = "";                                 // database password
+$root_path = "/var/www/example/hubcal/";  // full local path to hubcal, with trailing slash
+$ical_id = "hubcal.example.org";          // unique id for iCal feed- can be anything, but should contain your domain name
+
