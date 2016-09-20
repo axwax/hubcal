91d7ba4f5e1afa2d3aeb974b7f00f2edf11f22af
Axel Minet <axel@gigx.co.uk>
2016-09-20 10:29:15 +0100

    index: updated bootstrap to 3.3.7

--- index.php
+++ index.php
@@ -51,7 +51,7 @@ else {
 
 <link href='//cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.0.0/fullcalendar.min.css' rel='stylesheet' />
 <link href='//cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.0.0/fullcalendar.print.css' rel='stylesheet' media='print' />
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" />
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" />
 <link href="style.css" rel="stylesheet" />
 
 <link rel="icon" href="<?php echo $root; ?>images/calendar-icon-32x32.png" sizes="32x32" />
@@ -61,10 +61,10 @@ else {
 
 <script src='//cdnjs.cloudflare.com/ajax/libs/moment.js/2.15.0/moment.js'></script>
 <script src='//cdnjs.cloudflare.com/ajax/libs/moment.js/2.15.0/locale/en-gb.js'></script>
-<script src="https://code.jquery.com/jquery-3.1.0.min.js"   integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s="   crossorigin="anonymous"></script>
+<script src="//code.jquery.com/jquery-3.1.0.min.js"   integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s="   crossorigin="anonymous"></script>
 <script src='//cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.0.0/fullcalendar.min.js'></script>
 <script src='//cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.0.0/locale/en-gb.js'></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" ></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
 
 </head>
 <body>
